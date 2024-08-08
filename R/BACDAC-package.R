2065e45fb8860478ec15e5f302f19b9613cddf9b
Sarah H Johnson <johnson.sarah14@mayo.edu>
2024-08-08 17:00:24 -0500

    add files

---
+++ R/BACDAC-package.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices pdf
+#' @importFrom grDevices rgb
+#' @importFrom graphics abline
+#' @importFrom graphics axis
+#' @importFrom graphics mtext
+#' @importFrom graphics par
+#' @importFrom graphics points
+#' @importFrom graphics rect
+#' @importFrom graphics segments
+#' @importFrom graphics title
+#' @importFrom methods new
+#' @importFrom utils read.csv
+#' @importFrom utils write.csv
+## usethis namespace: end
