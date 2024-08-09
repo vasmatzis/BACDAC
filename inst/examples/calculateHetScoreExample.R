6b18468b4763497da13abab172c82e7874dc3045
Sarah H Johnson <johnson.sarah14@mayo.edu>
2024-08-09 16:03:03 -0500

    add extdata for example

---
+++ inst/examples/calculateHetScoreExample.R
@@ -0,0 +1,29 @@
+\dontrun{
+
+  library(BACDAC)
+  # calculateHetScoreExample.R
+  basicConfig("DEBUG")
+  sampleId='TCGA-14-1402-02A_ds'
+  inputDir <- system.file('extdata', package = "BACDAC")
+  dir(inputDir)
+  outputDir <- tempdir()
+
+  segmentationFile <- file.path(inputDir, paste0(sampleId, '_segmentation.csv'))
+  if(file.exists(segmentationFile)){
+    segmentation <- read.csv(segmentationFile,comment.char = '#', header = TRUE)
+
+    # check for required columns
+    requiredColumns=c('chr', 'start', 'end','rd')
+    missingColumnKey=which(!requiredColumns %in% names(segmentation))
+    if(length(missingColumnKey)>0){
+      logerror('missing required column: %s',requiredColumns[missingColumnKey])
+    }
+  }
+
+  calculateHetScore(
+    sampleId,
+    inputDir,
+    outputDir=outputDir,
+    segmentation=segmentation,
+    noPdf = TRUE)
+}
