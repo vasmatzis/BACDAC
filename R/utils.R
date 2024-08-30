f3d5866b1de66337ec9ded3ecb3861bc59b10f6d
Sarah H Johnson <johnson.sarah14@mayo.edu>
2024-08-30 17:35:39 -0500

    function to load reference files from zenodo

--- R/utils.R
+++ R/utils.R
@@ -27,3 +27,69 @@ checkSegmentation=function(segmentation){
 
     return(as.data.frame(segmentation[,returnColumns]) )
 }
+
+
+
+# https://zenodo.org/records/13619655
+
+#' load hsNormMat
+#'
+#' if file does not exist, will download from zenodo. Will check contents and return matrix if
+#' successfully loaded.
+#'
+#' @param hsNormMatFile full path to hsNormMatFile
+#' @export
+loadHsNormMat=function(hsNormMatFile){
+  if(!file.exists(hsNormMatFile)){
+    if(!dir.exists(dirname(destfile))){
+      dir.create(dirname(hsNormMatFile))
+    }
+    download.file(url='https://zenodo.org/records/13619655/files/hetScoreNormMat.Rds?download=1',
+                  destfile = hsNormMatFile)
+  }
+
+  # check again, should be there now.
+  if(!file.exists(hsNormMatFile)){
+    stop(sprintf('required file does not exist: %s:',hsNormMatFile ))
+  }else{
+    hsNormMat = readRDS(hsNormMatFile)
+    if(nrow(hsNormMat)==101046){
+      loginfo('%s loaded',basename(hsNormMatFile))
+    }else{
+      logwarn('expecting matrix with 101046 rows but found %i, file not loaded properly',
+              nrow(hsNormMat))
+    }
+  }
+  return(hsNormMat)
+}
+
+#' load testVals
+#'
+#' if file does not exist, will download from zenodo. Will check contents and return matrix if
+#' successfully loaded.
+#'
+#' @param testValsFile full path to testValsFile
+#' @export
+loadTestVals=function(testValsFile){
+  if(!file.exists(testValsFile)){
+    if(!dir.exists(dirname(destfile))){
+      dir.create(dirname(testValsFile))
+    }
+    download.file(url='https://zenodo.org/records/13619655/files/testVals.Rds?download=1',
+                  destfile = testValsFile)
+  }
+
+  # check again, should be there now.
+  if(!file.exists(testValsFile)){
+    stop(sprintf('required file does not exist: %s:',testValsFile ))
+  }else{
+    testVals = readRDS(testValsFile)
+    if(nrow(testVals)==997){
+      loginfo('%s loaded',basename(testValsFile))
+    }else{
+      logwarn('expecting matrix with 997 rows but found %i, file not loaded properly',
+              nrow(testVals))
+    }
+  }
+  return(testVals)
+}
